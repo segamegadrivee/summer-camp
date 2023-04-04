977585b9efabc6fdb3ceceadc7b0245dbfaf3c98
sega <sergohack2015@gmail.com>
2023-04-05 00:27:24 +0300

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import logo from '../assets/Logo1.svg';
+import facebook from '../assets/facebook.svg';
+import instagram from '../assets/instagram.svg';
 
 const Footer = () => {
     return (
@@ -6,28 +8,38 @@ const Footer = () => {
 
             <div className="footer__container container">
                 <div className="footer__location">
-                    <img className='footer__logo' src={logo} alt="logofooter" width={180} height={44} />
-                    <span>Location</span>
-                    <span>704 6 St NE Calgary, Alberta Canada</span>
-                    <a href="https://goo.gl/maps/qCRRCdG6Zu96nEmQ7">Go to map</a>
+                    <img className='footer__logo' src={logo} alt="logofooter" width={180} />
+
+                    <span className='footer__title' >Location</span>
+                    <span className='footer__text'>704 6 St NE Calgary, Alberta Canada</span>
+                    <a className='footer__maplink' href="https://goo.gl/maps/qCRRCdG6Zu96nEmQ7" target="_blank">Go to map</a>
                 </div>
+
 
                 <div className="footer__contact">
-                    <span>Contact phone number</span>
-                    <a href="tel:+14030000000">+1 (403) 000-0000</a>
-                    <a href="tel:+14030000000">+1 (403) 000-0000</a>
+                    <div className="footer__contact-number">
+                        <span className='footer__title'>Contact phone number</span>
+                        <a className='footer__text' href="tel:+14030000000">+1 (403) 000-0000</a>
+                        <a className='footer__text' href="tel:+14030000000">+1 (403) 000-0000</a>
 
-                    <span>Contact e-mail</span>
-                    <a href="mailto:summercapm@gmail.com">summercapm@gmail.com</a>
+                    </div>
+                    <div className="footer__contact-email">
+                        <span className='footer__title'>Contact e-mail</span>
+                        <a className='footer__text' href="mailto:summercapm@gmail.com">summercapm@gmail.com</a>
+                    </div>
                 </div>
 
+
                 <div className="footer__social">
-                    <span>Follow us</span>
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <a href="">About</a>
-                    <a href="">For Parents</a>
-                    <a href="">Volunteer work</a>
+                    <div className="footer__social-links">
+                        <span>Follow us</span>
+                        <a href="https://www.facebook.com/" target='_blank'><img src={facebook} alt="" /></a>
+                        <a href="https://www.instagram.com/" target='_blank'><img src={instagram} alt="" /></a>
+                    </div>
+
+                    <a className='footer__title' href="">About</a>
+                    <a className='footer__title' href="">For Parents</a>
+                    <a className='footer__title' href="">Volunteer work</a>
                 </div>
             </div>
 
