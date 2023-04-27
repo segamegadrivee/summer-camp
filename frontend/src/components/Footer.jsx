255112a57e38032f8b1e09e0188c49b61dcb3832
sega <sergohack2015@gmail.com>
2023-04-27 16:04:27 +0300

    footer redesign

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,27 +1,38 @@
 import logo from '../assets/Logo1.svg';
 import facebook from '../assets/facebook.svg';
 import instagram from '../assets/instagram.svg';
+import telegram from '../assets/telegram.svg';
 
 const Footer = () => {
     return (
         <footer>
 
             <div className="footer__container container">
-                <div className="footer__location">
+
+                <div className="footer__first">
                     <img className='footer__logo' src={logo} alt="logofooter" width={180} />
 
-                    <span className='footer__title' >Location</span>
-                    <span className='footer__text'>704 6 St NE Calgary, Alberta Canada</span>
-                    <a className='footer__maplink' href="https://goo.gl/maps/qCRRCdG6Zu96nEmQ7" target="_blank">Go to map</a>
+                    <div className="footer__social-links">
+                        <a href="https://www.facebook.com/abvmcalgary" target='_blank'><img src={facebook} alt="img" /></a>
+                        <a href="https://t.me/uagreekcatholicincalgary" target='_blank'><img className='test' src={telegram} alt="img" /></a>
+                    </div>
+
                 </div>
 
+
+                <div className="footer__social">
+                    <a className='footer__title' href="#">Home</a>
+                    <a className='footer__title' href="#">About</a>
+                    <a className='footer__title' href="#">For Parents</a>
+                    <a className='footer__title' href="#">Volunteer work</a>
+
+                    {/* <button className='common__button'>Register</button> */}
+                </div>
 
                 <div className="footer__contact">
                     <div className="footer__contact-number">
                         <span className='footer__title'>Contact phone number</span>
-                        <a className='footer__text' href="tel:+14030000000">+1 (403) 000-0000</a>
-                        <a className='footer__text' href="tel:+14030000000">+1 (403) 000-0000</a>
-
+                        <a className='footer__text' href="tel:+14032307013">+1 (403) 230-7013</a>
                     </div>
                     <div className="footer__contact-email">
                         <span className='footer__title'>Contact e-mail</span>
@@ -30,18 +41,11 @@ const Footer = () => {
                 </div>
 
 
-                <div className="footer__social">
-                    <div className="footer__social-links">
-                        <span className='footer__maplink'>Follow us</span>
-                        <a href="https://www.facebook.com/" target='_blank'><img src={facebook} alt="" /></a>
-                        <a href="https://www.instagram.com/" target='_blank'><img src={instagram} alt="" /></a>
-                    </div>
-
-                    <a className='footer__title' href="#">About</a>
-                    <a className='footer__title' href="#">For Parents</a>
-                    <a className='footer__title' href="#">Volunteer work</a>
-
-                    {/* <button className='common__button'>Register</button> */}
+                <div className="footer__location">
+                    <span className='footer__title' >Location</span>
+                    <span className='footer__text'>704 6 St NE Calgary,<br />
+                        Alberta Canada, T2E 3Y7</span>
+                    <a className='footer__maplink' href="https://goo.gl/maps/qCRRCdG6Zu96nEmQ7" target="_blank">Go to map</a>
                 </div>
 
 
