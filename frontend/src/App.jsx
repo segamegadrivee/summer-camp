bb669c940beb2484aa27f11d171ddb59c91cf652
sega <sergohack2015@gmail.com>
2023-05-06 22:13:43 +0300

    footer responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
             <Header />
             {/* <HomePage /> */}
             <Booking />
-            {/* <Footer /> */}
+            <Footer />
         </div>
     )
 }
