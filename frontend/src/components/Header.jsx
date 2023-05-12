da236b2eb1af5482faeade5f082fb1562a1219a7
sega <sergohack2015@gmail.com>
2023-05-12 23:21:53 +0300

    FINISHED MENU

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -37,13 +37,13 @@ const Header = () => {
             </div>
 
 
-            <nav >
+            <nav>
                 <ul className={`burger__menu${hamburgerOpen ? ' burger__menu--show' : ''}`} >
-                    <li><NavLink className='header__nav-item' to={'/'}>Home</NavLink></li>
-                    <li><NavLink className='header__nav-item' to={'/about'}>About</NavLink></li>
-                    <li><NavLink className='header__nav-item' to={'/parents'}>For Parents</NavLink></li>
-                    <li><NavLink className='header__nav-item' to={'/contact'} >Contact</NavLink></li>
-                    <li><NavLink className='header__nav-item' to={'/camps'}>Camps</NavLink></li>
+                    <li><NavLink className='header__nav-item' to={'/'} onClick={() => setHamburgerOpen(false)}>Home</NavLink></li>
+                    <li><NavLink className='header__nav-item' to={'/about'} onClick={() => setHamburgerOpen(false)}>About</NavLink></li>
+                    <li><NavLink className='header__nav-item' to={'/parents'} onClick={() => setHamburgerOpen(false)}>For Parents</NavLink></li>
+                    <li><NavLink className='header__nav-item' to={'/contact'} onClick={() => setHamburgerOpen(false)}>Contact</NavLink></li>
+                    <li><NavLink className='header__nav-item' to={'/camps'} onClick={() => setHamburgerOpen(false)}>Camps</NavLink></li>
                 </ul>
             </nav>
 
